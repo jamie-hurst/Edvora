f2e521162b63f2c737cd8965253bf2100165f603
warmrome <pxpx786@gmail.com>
2021-12-13 09:41:05 -0800

    Initial Commit

---
+++ Edvora/EdvoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraApp.swift
+//  Edvora
+//
+//  Created by Jameson Hurst on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
